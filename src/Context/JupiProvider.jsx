c50d806d1c65c22c5b3e7a0cc4af0d7f8da6f7ac
pipeswk <afco1995@gmail.com>
2022-06-04 15:05:36 -0500

    Se agregan consultas FB en tiempo real al Provider

--- src/Context/JupiProvider.jsx
+++ src/Context/JupiProvider.jsx
@@ -1,23 +1,71 @@
 import { createContext, useState, useEffect } from 'react';
+import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db } from '../../utils/Firebase';
 
 
 const JupiContext = createContext();
 
 const JupiProvider = ( { children } ) => {
 
-    const [pronosticoActual, setpronosticoActual] = useState({})
+    const [pronosticoActual, setpronosticoActual] = useState({});
+    const [sorteoActual, setSorteoActual] = useState({});
+    const [pronosticos, setPronosticos] = useState([]);
+    const [sorteos, setSorteos] = useState([]);
+
+    // Consulta en tiempo real de los pronosticos
+    const obtenerPronosticos = async () => {
+      const q = query(collection(db, "pronosticos"));
+      const consulta = onSnapshot(q, (querySnapshot) => {
+        const pronosticos = [];
+        querySnapshot.forEach((doc) => {
+            pronosticos.push({
+              id: doc.id,
+              ...doc.data()
+            });
+        });
+        setPronosticos(pronosticos);
+      });
+    }
+
+    // Consulta en tiempo real de los sorteos
+
+    const obtenerSorteos = async () => {
+      const q = query(collection(db, "sorteos"));
+      const consulta = onSnapshot(q, (querySnapshot) => {
+        const sorteos = [];
+        querySnapshot.forEach((doc) => {
+            sorteos.push({
+              id: doc.id,
+              ...doc.data()
+            });
+        });
+        setSorteos(sorteos);
+      });
+    }
+
+    useEffect(() => {
+      obtenerPronosticos();
+      obtenerSorteos();
+    }, []);
+    
 
 
     const cambiarPronostico = (pronostico) => {
         setpronosticoActual(pronostico);
     }
 
+    const cambiarSorteo = (sorteo) => {
+        setSorteoActual(sorteo);
+    }
+
 
     return (
         <JupiContext.Provider
             value={{
-              pronosticoActual,
-              cambiarPronostico
+              cambiarPronostico,
+              cambiarSorteo,
+              pronosticos,
+              sorteos
             }}
         >
             {children}
