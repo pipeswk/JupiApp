925a87a601ac83268ae0505c34a874e8fb3ef7a4
pipeswk <afco1995@gmail.com>
2022-06-04 15:04:54 -0500

    Se agrega formato moneda y categoria

--- src/Components/Sorteo.jsx
+++ src/Components/Sorteo.jsx
@@ -5,12 +5,15 @@ const Sorteo = ( { sorteo } ) => {
 
     const { categoria, nombre, valorTicket, nuevo, img } = sorteo
 
+    const moneda = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(valorTicket)
+
   return (
     <div className="card w-100 shadow-lg">
         <Image className="card-img-top" src={img} width={500} height={500} alt={`Imagen ${nombre}`} />
         <div className="card-body">
+            <p className='text-center'>{categoria}</p>
             <h4 className="card-title">{nombre}</h4>
-            <p className="card-text">{`Precio: ${valorTicket}`}</p>
+            <p className="card-text">{`Precio: ${moneda}`}</p>
         </div>
     </div>
   )
