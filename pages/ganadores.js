e2929471cef076ecb97462b50c9801f214d961d9
pipeswk <afco1995@gmail.com>
2022-06-07 00:30:19 -0500

    Pagina Ganadores v1

--- pages/ganadores.js
+++ pages/ganadores.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import TablaGanadores from '../src/Components/TablaGanadores'
+import WrapperGanadores from '../src/Components/WrapperGanadores'
 import Layout from '../src/Layout/Layout'
 
 const Ganadores = () => {
+
+
   return (
     <Layout
         pagina='Ganadores'
     >
-        Desde ganadores
+      <div className='mt-5'>
+        <h1 className='text-center fw-bold'>Ultimos Ganadores</h1>
+          <WrapperGanadores />
+      </div>
+      <div className='container mt-5'>
+        <TablaGanadores />
+      </div>
     </Layout>
   )
 }
