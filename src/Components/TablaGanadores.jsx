775022952cfaa410af15f0d54a3608921f801dda
pipeswk <afco1995@gmail.com>
2022-06-07 00:32:11 -0500

    Componente TablaGanadores v1

---
+++ src/Components/TablaGanadores.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import useJupi from '../Hooks/useJupi'
+
+const TablaGanadores = () => {
+
+    const { ganadores } = useJupi()
+
+  return (
+    <div>
+        <table className='table table-hover'>
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Ganador</th>
+            <th scope="col">Premio</th>
+            <th scope="col">Fecha entrega</th>
+            </tr>
+        </thead>
+        <tbody>
+            {ganadores.map( (ganador, index) => (
+                <tr key={index}>
+                    <th scope="row">{index}</th>
+                    <td>{ganador.nombre}</td>
+                    <td>{ganador.premio}</td>
+                    <td>06-06-2022</td>
+                </tr>
+            ))}
+        </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TablaGanadores
