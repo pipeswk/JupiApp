1cd1add2d77ae13680dadd14a3a481e18d5581d4
pipeswk <afco1995@gmail.com>
2022-06-07 00:30:57 -0500

    Nuevos estados y Formulario en componente

--- pages/sorteos/[id].js
+++ pages/sorteos/[id].js
@@ -4,6 +4,7 @@ import Layout from '../../src/Layout/Layout'
 import styles from '../../styles/EntradaPronostico.module.css'
 import { db } from '../../utils/Firebase'
 import { doc, getDoc } from "firebase/firestore";
+import FormSorteos from '../../src/Components/FormSorteos'
 
 const EntradaSorteo = ( { resultado, id } ) => {
 
@@ -11,10 +12,12 @@ const EntradaSorteo = ( { resultado, id } ) => {
     const [email, setEmail] = useState('');
     const [telefono, setTelefono] = useState('');
     const [metodo, setMetodo] = useState('');
-    const [numNequi, setNumNequi] = useState('')
+    const [numNequi, setNumNequi] = useState('');
+    const [cantidad, setCantidad] = useState('1');
   
     const { nombre, categoria, img, nuevo, valorTicket } = resultado
     const moneda = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(valorTicket);
+    const totalPagar = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(cantidad * valorTicket);
   
   
     const handleSubmit = (e) => {
@@ -47,10 +50,10 @@ const EntradaSorteo = ( { resultado, id } ) => {
                         <Image
                           src={img}
                           width={400}
-                          height={300}
+                          height={400}
                           // layout='responsive'
                           alt={nombre}
-                          className='rounded mx-auto'  
+                          className='rounded mx-auto img-fluid'  
                         />
                       </div>
                       {/* Cuerpo */}
@@ -68,106 +71,7 @@ const EntradaSorteo = ( { resultado, id } ) => {
                         {/* //TODO: validar precio a nivel del servidor */}
                         <p className='fs-4'><span  className='fw-bold'>Precio: </span>{moneda}</p>
                       </div>
-                      <form
-                        onSubmit={handleSubmit}
-                      >
-                        <div className="mb-3">
-                          <label htmlFor='nombre' className="form-label fw-bold">Nombre completo</label>
-                          <input
-                            type="text"
-                            className="form-control"
-                            id="nombre"
-                            placeholder="Ej: Andres Rojas"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                          />
-                        </div>
-  
-                        <div className="mb-3">
-                          <label htmlFor='email' className="form-label fw-bold">Email</label>
-                          <input
-                            type="email" 
-                            className="form-control" 
-                            id="email" 
-                            placeholder="Ej: correo@correo.com" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                          />
-                        </div>
-  
-                        <div className="mb-3">
-                          <label htmlFor='telefono' className="form-label fw-bold">Telefono</label>
-                          <div className="input-group mb-3">
-                            <span className="input-group-text" id="inputGroup-sizing-sm">+57</span>
-                            <input
-                              type="tel"
-                              id='telefono'
-                              className="form-control"
-                              placeholder='Ej: 3153315875'
-                              value={telefono}
-                              onChange={(e) => setTelefono(e.target.value)}
-                            />
-                          </div>
-                        </div>
-                        
-                        <p className='fw-bold'>Selecciona el metodo de pago (Disponible solo en Colombia):</p>
-                        <div className={styles.pagos}>
-                          <div
-                            id='NEQUI'
-                            className={metodo === 'NEQUI' ? styles.methodCard_Active : styles.methodCard_Desactive}
-                            onClick={() => setMetodo('NEQUI')}
-                          >
-                            <img src='/img/nequi.webp' className="img-fluid" alt="Logo Nequi"></img>
-                          </div>
-                          <div
-                            id='EFECTY'
-                            className={metodo === 'EFECTY' ? styles.methodCard_Active : styles.methodCard_Desactive}
-                            onClick={() => setMetodo('EFECTY')}
-                          >
-                            <img src='/img/efecty.png' className="img-fluid" alt="Logo Efecty"></img>
-                          </div>
-                          <div
-                            id='DAVIPLATA'
-                            className={metodo === 'DAVIPLATA' ? styles.methodCard_Active : styles.methodCard_Desactive}
-                            onClick={() => setMetodo('DAVIPLATA')}
-                          >
-                            <img src='/img/Daviplata.png' className="img-fluid" alt="Logo Efecty"></img>
-                          </div>
-                        </div>
-                        {metodo === 'NEQUI' && (
-                          <>
-                            <div className={styles.badge}>
-                              <p>Recibiras una notificacion en tu celular para realizar el pago con tu saldo de NEQUI</p>
-                            </div>
-                            <div className="mb-3">
-                              <label htmlFor='telnequi' className="form-label">Numero de NEQUI:</label>
-                              <div className="input-group mb-3">
-                                <span className="input-group-text" id="inputGroup-sizing-sm">+57</span>
-                                <input
-                                  type="tel"
-                                  id='telnequi'
-                                  className="form-control"
-                                  placeholder='Ej: 3153315875'
-                                  value={numNequi}
-                                  onChange={(e) => setNumNequi(e.target.value)}
-                                />
-                              </div>
-                            </div>
-                          </>
-                        )}
-                        {metodo === 'EFECTY' && (
-                          <>
-                            <div className={styles.badge}>
-                              <p>Obtendras un codigo que debes entregar al cajero en EFECTY para terminar tu pago</p>
-                            </div>
-                          </>
-                        )}
-  
-                        {/* //TODO: Falta el condicional de Daviplata */}
-                        
-  
-                        <button type='submit' className="btn btn-primary w-100 mt-5 fw-bold">PAGAR</button>
-                      </form>
+                      <FormSorteos />
                   </div>
                 </div>
               </div>
