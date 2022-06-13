d038e8b2c2c78fc592a644506f779f7cb021828a
pipeswk <afco1995@gmail.com>
2022-06-13 00:24:41 -0500

    Se agrega Formik y Yup

--- pages/pronosticos/[id].js
+++ pages/pronosticos/[id].js
@@ -4,32 +4,23 @@ import Layout from '../../src/Layout/Layout'
 import styles from '../../styles/EntradaPronostico.module.css'
 import { db } from '../../utils/Firebase'
 import { doc, getDoc } from "firebase/firestore";
+import FormPronosticos from '../../src/Components/FormPronosticos'
+import CountDown from '../../src/Components/CountDown'
 
 const EntradaPronostico = ( { resultado } ) => {
 
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [telefono, setTelefono] = useState('');
-  const [premio, setPremio] = useState('0');
-  const [metodo, setMetodo] = useState('');
-  const [numNequi, setNumNequi] = useState('')
-
-  const { nombre, categoria, img, sorteo, valorTicket } = resultado
+  const { nombre, descripcion, img, sorteo, valorTicket } = resultado
+  console.log(resultado)
   const moneda = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(valorTicket);
 
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log({
-      name,
-      email,
-      telefono,
-      premio,
-      metodo,
-      numNequi
-    })
-  }
+  const cuota = () => {
+    if(descripcion.tipo === 'Simple') {
+        return 'Reservada'
+    } else {
+        return descripcion.cuota
+    }
+}
 
 
   return (
@@ -60,7 +51,40 @@ const EntradaPronostico = ( { resultado } ) => {
                     </div>
                     {/* Cuerpo */}
 
-                    <p>Ipsum qui dolore occaecat culpa et exercitation exercitation quis Lorem non eu labore ex non. Consequat ea labore laboris in consectetur et proident voluptate do. Sunt ullamco aliquip eiusmod aliqua ad exercitation aute culpa ea fugiat voluptate cupidatat magna eiusmod.</p>
+                    <div>
+                      <div className='container'>
+                        <p className='text-center fs-5 fw-bold'>El pronostico se cerrara en</p>
+                        <CountDown />
+                        <div className='row mt-4'>
+                          <div className='col-12 col-md-6'>
+                            <p className='text-center fw-bold fs-5'>Descripcion del pronostico:</p>
+                            <div className='mt-3 p-md-3'>
+                            <p className={styles.parrafo}><span className='fw-bold'>Cuota: </span>{cuota()}</p>
+                            <p className={styles.parrafo}><span className='fw-bold'>Stake: </span>{descripcion.stake}</p>
+                            <p className={styles.parrafo}><span className='fw-bold'>Tipo: </span>{descripcion.tipo}</p>
+                          </div>
+                          </div>
+                          <div className='col-12 col-md-6 p-3'>
+                            <div>
+                              <Image src='/img/Sorteo.gif' width={350} height={550} alt='logo-bet' className='img-fluid rounded-3' />
+                            </div>
+                            <p className='text-decoration-underline fw-bold'>Instrucciones de compra:</p>
+                            <ol className='d-md-none'>
+                              <li>Llena el formulario de <span className='fw-bold'>abajo</span></li>
+                              <li>Elige el sorteo al que deseas participar con tu compra <span className='fw-bold'>(Incluido)</span></li>
+                              <li>Selecciona el metodo de pago de tu preferencia</li>
+                              <li>Haz clic en el boton "PAGAR" y sigue las instrucciones</li>
+                            </ol>
+                            <ol className='d-none d-md-block'>
+                              <li>Llena el formulario del lado <span className='fw-bold'>derecho</span></li>
+                              <li>Elige el sorteo al que deseas participar con tu compra <span className='fw-bold'>(Incluido)</span></li>
+                              <li>Selecciona el metodo de pago de tu preferencia</li>
+                              <li>Haz clic en el boton "PAGAR" y sigue las instrucciones</li>
+                            </ol>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
                   </div>
                 </div>
@@ -77,120 +101,7 @@ const EntradaPronostico = ( { resultado } ) => {
                       {/* //TODO: validar precio a nivel del servidor */}
                       <p className='fs-4'><span  className='fw-bold'>Precio: </span>{moneda}</p>
                     </div>
-                    <form
-                      onSubmit={handleSubmit}
-                    >
-                      <div className="mb-3">
-                        <label htmlFor='nombre' className="form-label fw-bold">Nombre completo</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="nombre"
-                          placeholder="Ej: Andres Rojas"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                        />
-                      </div>
-
-                      <div className="mb-3">
-                        <label htmlFor='email' className="form-label fw-bold">Email</label>
-                        <input
-                          type="email" 
-                          className="form-control" 
-                          id="email" 
-                          placeholder="Ej: correo@correo.com" 
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                      </div>
-
-                      <div className="mb-3">
-                        <label htmlFor='telefono' className="form-label fw-bold">Telefono</label>
-                        <div className="input-group mb-3">
-                          <span className="input-group-text" id="inputGroup-sizing-sm">+57</span>
-                          <input
-                            type="tel"
-                            id='telefono'
-                            className="form-control"
-                            placeholder='Ej: 3153315875'
-                            value={telefono}
-                            onChange={(e) => setTelefono(e.target.value)}
-                          />
-                        </div>
-                      </div>
-
-                      <div className="mb-3">
-                        <label htmlFor='sorteo' className="form-label fw-bold">Elige el sorteo en el que deseas participar</label>
-                        <select
-                          className="form-select"
-                          id="sorteo"
-                          value={premio}
-                          onChange={(e) => setPremio(e.target.value)}
-                        >
-                          <option value='0'>--Seleccionar--</option>
-                          {/* //TODO: Mapear disponibles */}
-                          <option value="2">2</option>
-                        </select>
-                      </div>
-                      
-                      <p className='fw-bold'>Selecciona el metodo de pago (Disponible solo en Colombia):</p>
-                      <div className={styles.pagos}>
-                        <div
-                          id='NEQUI'
-                          className={metodo === 'NEQUI' ? styles.methodCard_Active : styles.methodCard_Desactive}
-                          onClick={() => setMetodo('NEQUI')}
-                        >
-                          <img src='/img/nequi.webp' className="img-fluid" alt="Logo Nequi"></img>
-                        </div>
-                        <div
-                          id='EFECTY'
-                          className={metodo === 'EFECTY' ? styles.methodCard_Active : styles.methodCard_Desactive}
-                          onClick={() => setMetodo('EFECTY')}
-                        >
-                          <img src='/img/efecty.png' className="img-fluid" alt="Logo Efecty"></img>
-                        </div>
-                        <div
-                          id='DAVIPLATA'
-                          className={metodo === 'DAVIPLATA' ? styles.methodCard_Active : styles.methodCard_Desactive}
-                          onClick={() => setMetodo('DAVIPLATA')}
-                        >
-                          <img src='/img/Daviplata.png' className="img-fluid" alt="Logo Efecty"></img>
-                        </div>
-                      </div>
-                      {metodo === 'NEQUI' && (
-                        <>
-                          <div className={styles.badge}>
-                            <p>Recibiras una notificacion en tu celular para realizar el pago con tu saldo de NEQUI</p>
-                          </div>
-                          <div className="mb-3">
-                            <label htmlFor='telnequi' className="form-label">Numero de NEQUI:</label>
-                            <div className="input-group mb-3">
-                              <span className="input-group-text" id="inputGroup-sizing-sm">+57</span>
-                              <input
-                                type="tel"
-                                id='telnequi'
-                                className="form-control"
-                                placeholder='Ej: 3153315875'
-                                value={numNequi}
-                                onChange={(e) => setNumNequi(e.target.value)}
-                              />
-                            </div>
-                          </div>
-                        </>
-                      )}
-                      {metodo === 'EFECTY' && (
-                        <>
-                          <div className={styles.badge}>
-                            <p>Obtendras un codigo que debes entregar al cajero en EFECTY para terminar tu pago</p>
-                          </div>
-                        </>
-                      )}
-
-                      {/* //TODO: Falta el condicional de Daviplata */}
-                      
-
-                      <button type='submit' className="btn btn-primary w-100 mt-5 fw-bold">PAGAR</button>
-                    </form>
+                    <FormPronosticos moneda={moneda} />
                 </div>
               </div>
             </div>
