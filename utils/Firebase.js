6dad967509faa8e2f0136d01748e24688c95a408
pipeswk <afco1995@gmail.com>
2022-06-21 17:01:37 -0500

    Cambios menores

--- utils/Firebase.js
+++ utils/Firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
-  // const analytics = getAnalytics(app);
+  const functions = getFunctions(app);
 
   export {
     db
