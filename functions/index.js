42aa55d137b7a06d8e1fd12eccbe5f6f9b793b83
pipeswk <afco1995@gmail.com>
2022-06-21 16:55:43 -0500

    Se configrua express, cors, dotenv y rutas

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,39 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-const admin = require("firebase-admin");
-admin.initializeApp();
+const app = express();
+const eventos = express();
 
+dotenv.config();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// cors
+
+const whitelist = [
+  process.env.FRONTEND1,
+  process.env.FRONTEND2,
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    console.log(origin);
+    if (whitelist.includes(origin)) {
+      // Puede consultar la API
+      callback(null, true);
+    } else {
+      // No esta permitido el request desde ese origen
+      callback(new Error("Error de Cors"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+// Routing
+
+app.use("/api/nequi", require("./routes/nequi.routes.js"));
+eventos.use("/", require("./routes/eventos.routes.js"));
+
+exports.app = functions.https.onRequest(app);
+exports.eventos = functions.https.onRequest(eventos);
