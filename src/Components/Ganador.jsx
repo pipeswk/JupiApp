a6795408881b04389607bc160d1783cc31a352e2
pipeswk <afco1995@gmail.com>
2022-06-21 16:59:35 -0500

    Se agregan nuevas propiedades a la carta

--- src/Components/Ganador.jsx
+++ src/Components/Ganador.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import Image from 'next/image';
 import styles from '../../styles/Ganador.module.css'
 
 const Ganador = ( { ganador } ) => {
 
-    const { nombre, premio, img_src } = ganador;
+    const { nombre, premio, img_src, ciudad, valor_premio } = ganador;
+    const moneda = new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(valor_premio)
 
   return (
     <div className="col-11 col-md-4 col-lg-3 card mx-1">
-        <img className={`${styles.image} card-img-top img-fluid`} src={img_src} alt="Title" />
+        {/* <img className={`${styles.image} card-img-top img-fluid`} src={img_src} alt="Title" /> */}
+        <Image width={800} height={1000} className={`${styles.image} card-img-top img-fluid`} src={img_src} alt="Title" />
         <div className="card-body">
             <h4 className="card-title text-center">{nombre}</h4>
             <p className="card-text text-break"><span className='fw-bold text-break'>🔥Premio: </span>{premio}</p>
+            <p className="card-text text-break"><span className='fw-bold text-break'>🏢Ciudad: </span>{ciudad}</p>
+            <p className="card-text text-break"><span className='fw-bold text-break'>💲Valor premio: </span>{moneda}</p>
         </div>
     </div>
   )
