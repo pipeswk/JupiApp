d6a9e9ddd16f086c174ce9de4dabf4b02c84c8e1
pipeswk <afco1995@gmail.com>
2022-06-29 17:39:14 -0500

    Se renombran los callback

--- functions/routes/nequi.routes.js
+++ functions/routes/nequi.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
-  ejecutarPago,
+  ejecutarPagoSorteo,
+  ejecutarPagoPronostico,
 } = require("../controllers/nequiController.js");
 
 const router = new express.Router();
 
-router.post("/procesar-pago-sorteo", ejecutarPago);
+router.post("/procesar-pago-sorteo", ejecutarPagoSorteo);
+router.post("/procesar-pago-pronostico", ejecutarPagoPronostico);
 
 module.exports = router;
