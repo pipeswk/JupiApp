b6b0996e3761a888fce13532cc33496b19c0458f
pipeswk <afco1995@gmail.com>
2022-06-29 17:41:20 -0500

    Se hace dinamica la barra de progreso

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,47 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-const Progress = () => {
+const Progress = ( { data } ) => {
 
-    const [porcentaje, setPorcentaje] = useState(75);
+    const [porcentaje, setPorcentaje] = useState(0);
 
-    const participantes = [
-        {
-            nombre: 'Juan'
-        },
-        {
-            nombre: 'Pedro'
-        },
-        {
-            nombre: 'Maria'
-        },
-        {
-            nombre: 'Felipe'
+    useEffect(() => {
+        const calcularPorcentaje = () => {
+            const porc = Math.round((data?.participantes.length / 500) * 100);
+            setPorcentaje(porc);
+            console.log(porcentaje);
         }
-    ]
-
-    // const calcularPorcentaje = () => {
-    //     setPorcentaje((participantes.length / 2000) * 100);
-    //     console.log(porcentaje);
-    // }
-    // calcularPorcentaje();
+        calcularPorcentaje();
+    }, [data])
 
   return (
-    <div className='progress'>
-        <div
-            className={porcentaje === 100 ? (
-                'progress-bar progress-bar-striped progress-bar-animated bg-success fw-bold'
-            ) : (
-                'progress-bar progress-bar-striped progress-bar-animated fw-bold'
-            )}
-            role="progressbar"
-            aria-valuenow="75"
-            aria-valuemin="0"
-            aria-valuemax="100"
-            style={{
-                width: `${porcentaje}%`
-            }}
-        >
-            {porcentaje}%
+    <div>
+        <p className='text-center fw-bold'>{`Cupos Disponibles: ${500 - data?.participantes.length}`}</p>
+        <div className='progress'>
+            <div
+                className={porcentaje === 100 ? (
+                    'progress-bar progress-bar-striped progress-bar-animated bg-success fw-bold'
+                ) : (
+                    'progress-bar progress-bar-striped progress-bar-animated fw-bold'
+                )}
+                role="progressbar"
+                aria-valuenow="75"
+                aria-valuemin="0"
+                aria-valuemax="100"
+                style={{
+                    width: `${porcentaje}%`
+                }}
+            >
+                {porcentaje}%
+            </div>
         </div>
     </div>
   )
