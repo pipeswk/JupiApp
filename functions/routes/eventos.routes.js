1c1f91e4205d5cbfd13dfa3719ac88b92f8aa2c8
pipeswk <afco1995@gmail.com>
2022-06-29 17:38:56 -0500

    Nuevo endpoint MercadoPago

--- functions/routes/eventos.routes.js
+++ functions/routes/eventos.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   escucharEventos,
+  eventosMercadoPago,
 } = require("../controllers/eventosController.js");
 
 const router = new express.Router();
 
 router.post("/nequi", escucharEventos);
+router.post("/mercadopago", eventosMercadoPago);
 
 module.exports = router;
