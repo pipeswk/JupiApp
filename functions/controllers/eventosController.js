21682f666197b9fb28c41aaf1aba15aadba5cc7f
pipeswk <afco1995@gmail.com>
2022-06-29 17:37:39 -0500

    Se agrega participantes al sorteo, nuevo endpoint mp

--- functions/controllers/eventosController.js
+++ functions/controllers/eventosController.js
@@ -4,24 +4,50 @@ const db = admin.firestore();
 // Configuracion de end-point para escuchar eventos de la API de Nequi
 
 const escucharEventos = async (req, res) => {
-  res.status(200).send({
-    message: "Evento escuchado",
-  });
   if (req.body.data.transaction.status === "APPROVED") {
     try {
       const docRef = db.collection("transactions")
           .doc(req.body.data.transaction.reference);
+      const documento = await docRef.get();
       await docRef.update({
         statusTransaccion: true,
+      });
+      // Se alimenta el Array del Sorteo
+      const nuevosInscritos = [];
+      for (let i = 0; i < documento.data().cantidad; i++) {
+        nuevosInscritos.push(`${documento.data().refPago}-${i}`);
+      }
+
+      const sortRef = db.collection("sorteos")
+          .doc(documento.data().idSorteo);
+      await sortRef.update({
+        participantes: admin.firestore.FieldValue
+            .arrayUnion(...nuevosInscritos),
+      });
+
+      res.status(200).send({
+        message: "Evento escuchado",
       });
     } catch (error) {
       console.error(error);
     }
-    return;
+  } else {
+    res.status(200).send({
+      message: "Evento escuchado, transaccion no completada.",
+    });
   }
 };
 
+// Configuracion de end-point para escuchar eventos de la API de Nequi
+
+const eventosMercadoPago = async (req, res) => {
+  console.log(req.body);
+  res.status(200).send({
+    message: "Evento escuchado",
+  });
+};
 
 module.exports = {
   escucharEventos,
+  eventosMercadoPago,
 };
