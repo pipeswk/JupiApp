a6f78863a217c3354344f05cb7dece0b8705b75a
pipeswk <afco1995@gmail.com>
2022-06-29 17:41:31 -0500

    Componente Spinner v1

---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,17 @@
+import styles from '../../styles/Spinner.module.css'
+
+const Spinner = () => {
+
+  return (
+    <div className={styles.sk_chase}>
+        <div className={styles.sk_chase_dot}></div>
+        <div className={styles.sk_chase_dot}></div>
+        <div className={styles.sk_chase_dot}></div>
+        <div className={styles.sk_chase_dot}></div>
+        <div className={styles.sk_chase_dot}></div>
+        <div className={styles.sk_chase_dot}></div>
+    </div>
+  )
+}
+
+export default Spinner
