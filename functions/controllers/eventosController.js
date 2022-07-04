17c4290355c73b0e9468b86c68834e5a7d7e1727
pipeswk <afco1995@gmail.com>
2022-07-04 13:24:34 -0500

    Nuevo evento MercadoPago

--- functions/controllers/eventosController.js
+++ functions/controllers/eventosController.js
@@ -1,4 +1,5 @@
 const admin = require("firebase-admin");
+const axios = require("axios");
 const db = admin.firestore();
 
 // Configuracion de end-point para escuchar eventos de la API de Nequi
@@ -38,13 +39,64 @@ const escucharEventos = async (req, res) => {
   }
 };
 
-// Configuracion de end-point para escuchar eventos de la API de Nequi
+// Configuracion de end-point para escuchar eventos de la API de MercadoPago
 
 const eventosMercadoPago = async (req, res) => {
   console.log(req.body);
-  res.status(200).send({
-    message: "Evento escuchado",
-  });
+  if (req.body.action === "payment.updated") {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.ACCESS_TOKEN_MP}`,
+        },
+      };
+      const url = `https://api.mercadopago.com/v1/payments/${req.body.data.id}`;
+      const {data} = await axios.get(url, config);
+      if (data.status === "approved") {
+        const id = req.body.data.id;
+        const ref = db.collection("transactions");
+        const snap = await ref.where("idMercadoPago", "==", parseInt(id)).get();
+        if (snap.empty) {
+          console.log(`No se encontro el id ${req.body.data.id}`);
+          res.status(200).send({
+            message: "Evento escuchado",
+          });
+          return;
+        }
+        const docs = [];
+        snap.forEach((doc) => {
+          console.log(doc.id, "=>", doc.data());
+          docs.push(doc.data());
+        });
+        const docRef = db.collection("transactions").doc(docs[0].refPago);
+        await docRef.update({
+          statusTransaccion: true,
+        });
+        // Se alimenta el Array del Sorteo
+        const nuevosInscritos = [];
+        for (let i = 0; i < docs[0].cantidad; i++) {
+          nuevosInscritos.push(`${docs[0].refPago}-${i}`);
+        }
+
+        const sortRef = db.collection("sorteos")
+            .doc(docs[0].idSorteo);
+        await sortRef.update({
+          participantes: admin.firestore.FieldValue
+              .arrayUnion(...nuevosInscritos),
+        });
+        res.status(200).send({
+          message: "Evento escuchado",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(200).send({
+      message: "Evento escuchado",
+    });
+  }
 };
 
 module.exports = {
