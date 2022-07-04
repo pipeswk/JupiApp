3953b7bea8fc23d0bfa4c4efe0f62d97e9fb8f17
pipeswk <afco1995@gmail.com>
2022-07-04 13:24:51 -0500

    Nuevo EndPoint PSE

--- functions/routes/efectivo.routes.js
+++ functions/routes/efectivo.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   efecty,
+  pse,
 } = require("../controllers/efectivoController.js");
 
 const router = new express.Router();
 
 router.post("/efecty", efecty);
+router.post("/pse", pse);
 
 module.exports = router;
