f4d37e8d62a36a017fb2456d6ad73a9561d62697
pipeswk <afco1995@gmail.com>
2022-07-04 13:24:17 -0500

    Se agrega endpoint PSE

--- functions/controllers/efectivoController.js
+++ functions/controllers/efectivoController.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const db = admin.firestore();
 const mercadopago = require("mercadopago");
-mercadopago.configurations.setAccessToken(process.env.ACCESS_TOKEN_MP_2);
+mercadopago.configurations.setAccessToken(process.env.ACCESS_TOKEN_MP);
 
 const efecty = async (req, res) => {
   console.log(req.body);
@@ -94,6 +94,115 @@ const efecty = async (req, res) => {
   }
 };
 
+const pse = async (req, res) => {
+  console.log(req.body);
+  if (req.body.product === "sorteo") {
+    const documento = await db.collection("transactions").add({
+      nombreCliente: req.body.data.nombre,
+      refPago: "",
+      method: req.body.data.method,
+      tipoCompra: req.body.product,
+      idSorteo: req.body.data.idSorteo,
+      cantidad: req.body.data.cantidad,
+      telefono: req.body.data.telefono,
+      email: req.body.data.email,
+      statusTransaccion: false,
+      transaccionCreada: false,
+    });
+    const sortRef = db.collection("sorteos").doc(req.body.data.idSorteo);
+    const sort = await sortRef.get();
+    const precioFinal = sort.data().valorTicket * req.body.data.cantidad;
+    const paymentData = {
+      transaction_amount: precioFinal,
+      description: `Sorteo ${sort.data().nombre}`,
+      payment_method_id: "pse",
+      payer: {
+        email: req.body.data.email,
+        identification: {
+          type: req.body.data.tipoDocumento,
+          number: toString(req.body.data.noDocumento),
+        },
+        entity_type: "individual",
+      },
+      transaction_details: {
+        financial_institution: req.body.data.entidad,
+      },
+      additional_info: {
+        ip_address: req.body.ip,
+      },
+      callback_url: "http://www.tu-sitio.com", // TODO: Cambiar a tu dominio
+      notification_url: "https://us-central1-jupi-e46aa.cloudfunctions.net/eventos/mercadopago",
+    };
+    mercadopago.payment.create(paymentData).then(function(data) {
+      console.log(data);
+      const docRef = db.collection("transactions").doc(documento.id);
+      docRef.update({
+        refPago: documento.id,
+        valorTransaccion: precioFinal,
+        transaccionCreada: true,
+        idMercadoPago: data.response.id,
+      });
+      res.status(200).send(data);
+    }).catch(function(error) {
+      console.log(error);
+      res.status(500).send(error);
+    });
+  } else {
+    const documento = await db.collection("transactions").add({
+      nombreCliente: req.body.data.nombre,
+      refPago: "",
+      method: req.body.data.method,
+      tipoCompra: req.body.product,
+      idProductoComprado: req.body.data.idProdComprado,
+      cantidad: 1,
+      telefono: req.body.data.telefono,
+      email: req.body.data.email,
+      idSorteo: req.body.data.sorteo,
+      statusTransaccion: false,
+      transaccionCreada: false,
+    });
+    const proRef = db.collection("pronosticos")
+        .doc(req.body.data.idProdComprado);
+    const pron = await proRef.get();
+    const precioFinal = pron.data().valorTicket * 1;
+    const paymentData = {
+      transaction_amount: precioFinal,
+      description: `Pronostico ${pron.data().nombre}`,
+      payment_method_id: "pse",
+      payer: {
+        email: req.body.data.email,
+        identification: {
+          type: req.body.data.tipoDocumento,
+          number: toString(req.body.data.noDocumento),
+        },
+        entity_type: "individual",
+      },
+      transaction_details: {
+        financial_institution: req.body.data.entidad,
+      },
+      additional_info: {
+        ip_address: req.body.ip,
+      },
+      callback_url: "http://www.tu-sitio.com", // TODO: Cambiar a tu dominio
+    };
+    mercadopago.payment.create(paymentData).then(function(data) {
+      console.log(data);
+      const docRef = db.collection("transactions").doc(documento.id);
+      docRef.update({
+        refPago: documento.id,
+        valorTransaccion: precioFinal,
+        transaccionCreada: true,
+        idMercadoPago: data.response.id,
+      });
+      res.status(200).send(data);
+    }).catch(function(error) {
+      console.log(error);
+      res.status(500).send(error);
+    });
+  }
+};
+
 module.exports = {
   efecty,
+  pse,
 };
