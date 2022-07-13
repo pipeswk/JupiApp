04001f8a2ecc1e59d895089974c7fb3868788dc4
pipeswk <afco1995@gmail.com>
2022-07-13 00:22:19 -0500

    Se comenta funcion de whatsapp

--- functions/controllers/eventosController.js
+++ functions/controllers/eventosController.js
@@ -25,75 +25,78 @@ const escucharEventos = async (req, res) => {
         participantes: admin.firestore.FieldValue
             .arrayUnion(...nuevosInscritos),
       });
-
-      // Se envia mensaje de whatsapp
-
-      const whatsappData = JSON.stringify({
-        "messaging_product": "whatsapp",
-        "to": `57${documento.data().telefono}`,
-        "type": "template",
-        "template": {
-          "name": "sort_normal",
-          "language": {
-            "code": "es",
-          },
-          "components": [
-            {
-              "type": "header",
-              "parameters": [
-                {
-                  "type": "image",
-                  "image": {
-                    "link": "https://firebasestorage.googleapis.com/v0/b/jupi-e46aa.appspot.com/o/Sorteos%2FKTM-DUKE.jpg?alt=media&token=b646a623-5928-465b-a111-79b53a8ebdcc",
-                  },
-                },
-              ],
-            },
-            {
-              "type": "body",
-              "parameters": [
-                {
-                  "type": "text",
-                  "text": documento.data().nombreCliente,
-                },
-                {
-                  "type": "text",
-                  "text": "sorteo",
-                },
-                {
-                  "type": "text",
-                  "text": "KTM - DUKE 390",
-                },
-                {
-                  "type": "text",
-                  "text": documento.data().cantidad,
-                },
-                {
-                  "type": "text",
-                  "text": "HbjhbHJBhjb-0",
-                },
-                {
-                  "type": "text",
-                  "text": "HbjhbHJBhjb-1",
-                },
-              ],
-            },
-          ],
-        },
-      });
-
-      const whatsappConfig = {
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
-        },
-      };
-      const url = "https://graph.facebook.com/v13.0/110412985060402/messages";
-      const {data} = await axios.post(url, whatsappData, whatsappConfig);
-      console.log(data);
       res.status(200).send({
         message: "Evento escuchado",
       });
+
+      // Se envia mensaje de whatsapp
+
+      // const whatsappData = JSON.stringify({
+      //   "messaging_product": "whatsapp",
+      //   "to": `57${documento.data().telefono}`,
+      //   "type": "template",
+      //   "template": {
+      //     "name": "sort_normal",
+      //     "language": {
+      //       "code": "es",
+      //     },
+      //     "components": [
+      //       {
+      //         "type": "header",
+      //         "parameters": [
+      //           {
+      //             "type": "image",
+      //             "image": {
+      //               "link": "https://firebasestorage.googleapis.com/v0/b/jupi-e46aa.appspot.com/o/Sorteos%2FKTM-DUKE.jpg?alt=media&token=b646a623-5928-465b-a111-79b53a8ebdcc",
+      //             },
+      //           },
+      //         ],
+      //       },
+      //       {
+      //         "type": "body",
+      //         "parameters": [
+      //           {
+      //             "type": "text",
+      //             "text": documento.data().nombreCliente,
+      //           },
+      //           {
+      //             "type": "text",
+      //             "text": "sorteo",
+      //           },
+      //           {
+      //             "type": "text",
+      //             "text": "KTM - DUKE 390",
+      //           },
+      //           {
+      //             "type": "text",
+      //             "text": documento.data().cantidad,
+      //           },
+      //           {
+      //             "type": "text",
+      //             "text": "HbjhbHJBhjb-0",
+      //           },
+      //           {
+      //             "type": "text",
+      //             "text": "HbjhbHJBhjb-1",
+      //           },
+      //         ],
+      //       },
+      //     ],
+      //   },
+      // });
+
+      // const whatsappConfig = {
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //     "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
+      //   },
+      // };
+      // const url = "https://graph.facebook.com/v13.0/110412985060402/messages";
+      // const {data} = await axios.post(url, whatsappData, whatsappConfig);
+      // console.log(data);
+      // res.status(200).send({
+      //   message: "Evento escuchado",
+      // });
     } catch (error) {
       console.error(error);
       res.end();
