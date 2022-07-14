d8c3b1a36418c479823edc9088e2e5e213279c73
pipeswk <afco1995@gmail.com>
2022-07-13 22:31:00 -0500

    Se configura whatsapp API en evento de Nequi

--- functions/controllers/eventosController.js
+++ functions/controllers/eventosController.js
@@ -19,84 +19,157 @@ const escucharEventos = async (req, res) => {
         nuevosInscritos.push(`${documento.data().refPago}-${i}`);
       }
 
-      const sortRef = db.collection("sorteos")
-          .doc(documento.data().idSorteo);
-      await sortRef.update({
-        participantes: admin.firestore.FieldValue
-            .arrayUnion(...nuevosInscritos),
-      });
-      res.status(200).send({
-        message: "Evento escuchado",
-      });
-
-      // Se envia mensaje de whatsapp
-
-      // const whatsappData = JSON.stringify({
-      //   "messaging_product": "whatsapp",
-      //   "to": `57${documento.data().telefono}`,
-      //   "type": "template",
-      //   "template": {
-      //     "name": "sort_normal",
-      //     "language": {
-      //       "code": "es",
-      //     },
-      //     "components": [
-      //       {
-      //         "type": "header",
-      //         "parameters": [
-      //           {
-      //             "type": "image",
-      //             "image": {
-      //               "link": "https://firebasestorage.googleapis.com/v0/b/jupi-e46aa.appspot.com/o/Sorteos%2FKTM-DUKE.jpg?alt=media&token=b646a623-5928-465b-a111-79b53a8ebdcc",
-      //             },
-      //           },
-      //         ],
-      //       },
-      //       {
-      //         "type": "body",
-      //         "parameters": [
-      //           {
-      //             "type": "text",
-      //             "text": documento.data().nombreCliente,
-      //           },
-      //           {
-      //             "type": "text",
-      //             "text": "sorteo",
-      //           },
-      //           {
-      //             "type": "text",
-      //             "text": "KTM - DUKE 390",
-      //           },
-      //           {
-      //             "type": "text",
-      //             "text": documento.data().cantidad,
-      //           },
-      //           {
-      //             "type": "text",
-      //             "text": "HbjhbHJBhjb-0",
-      //           },
-      //           {
-      //             "type": "text",
-      //             "text": "HbjhbHJBhjb-1",
-      //           },
-      //         ],
-      //       },
-      //     ],
-      //   },
-      // });
-
-      // const whatsappConfig = {
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
-      //   },
-      // };
-      // const url = "https://graph.facebook.com/v13.0/110412985060402/messages";
-      // const {data} = await axios.post(url, whatsappData, whatsappConfig);
-      // console.log(data);
-      // res.status(200).send({
-      //   message: "Evento escuchado",
-      // });
+      if (documento.data().tipoCompra === "sorteo") {
+        const sortRef = db.collection("sorteos")
+            .doc(documento.data().idSorteo);
+        const sorteo = await sortRef.get();
+        await sortRef.update({
+          participantes: admin.firestore.FieldValue
+              .arrayUnion(...nuevosInscritos),
+        });
+        // Se envia mensaje de whatsapp
+        const whatsappData = JSON.stringify({
+          "messaging_product": "whatsapp",
+          "to": `57${documento.data().telefono}`,
+          "type": "template",
+          "template": {
+            "name": "sort_with_img",
+            "language": {
+              "code": "es_MX",
+            },
+            "components": [
+              {
+                "type": "header",
+                "parameters": [
+                  {
+                    "type": "image",
+                    "image": {
+                      "link": sorteo.data().img,
+                    },
+                  },
+                ],
+              },
+              {
+                "type": "body",
+                "parameters": [
+                  {
+                    "type": "text",
+                    "text": documento.data().nombreCliente,
+                  },
+                  {
+                    "type": "text",
+                    "text": documento.data().tipoCompra,
+                  },
+                  {
+                    "type": "text",
+                    "text": sorteo.data().nombre,
+                  },
+                  {
+                    "type": "text",
+                    "text": documento.data().cantidad,
+                  },
+                  {
+                    "type": "text",
+                    "text": documento.data().refPago,
+                  },
+                ],
+              },
+            ],
+          },
+        });
+        const whatsappConfig = {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
+          },
+        };
+        const url = "https://graph.facebook.com/v13.0/106635852120380/messages";
+        const {data} = await axios.post(url, whatsappData, whatsappConfig);
+        console.log(data);
+        res.status(200).send({
+          message: "Evento escuchado",
+        });
+      } else {
+        const sortRef = db.collection("sorteos")
+            .doc(documento.data().idSorteo);
+        const sorteo = await sortRef.get();
+        await sortRef.update({
+          participantes: admin.firestore.FieldValue
+              .arrayUnion(...nuevosInscritos),
+        });
+        const pronRef = db.collection("pronosticos")
+            .doc(documento.data().idProductoComprado);
+        await pronRef.update({
+          compradores: admin.firestore.FieldValue
+              .arrayUnion(...nuevosInscritos),
+        });
+        // Se envia mensaje de whatsapp
+        const whatsappData = JSON.stringify({
+          "messaging_product": "whatsapp",
+          "to": `57${documento.data().telefono}`,
+          "type": "template",
+          "template": {
+            "name": "pron_with_img",
+            "language": {
+              "code": "es_MX",
+            },
+            "components": [
+              {
+                "type": "header",
+                "parameters": [
+                  {
+                    "type": "image",
+                    "image": {
+                      "link": "https://firebasestorage.googleapis.com/v0/b/jupi-e46aa.appspot.com/o/pronosticos%2FNHL.jpg?alt=media&token=276807f0-b780-460c-80a2-94f526300e17",
+                    },
+                  },
+                ],
+              },
+              {
+                "type": "body",
+                "parameters": [
+                  {
+                    "type": "text",
+                    "text": documento.data().nombreCliente,
+                  },
+                  {
+                    "type": "text",
+                    "text": documento.data().tipoCompra,
+                  },
+                  {
+                    "type": "text",
+                    "text": "sorteo",
+                  },
+                  {
+                    "type": "text",
+                    "text": sorteo.data().nombre,
+                  },
+                  {
+                    "type": "text",
+                    "text": documento.data().refPago,
+                  },
+                  {
+                    "type": "text",
+                    "text": "ganar",
+                  },
+                ],
+              },
+            ],
+          },
+        });
+        const whatsappConfig = {
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
+          },
+        };
+        const url = "https://graph.facebook.com/v13.0/106635852120380/messages";
+        const {data} = await axios.post(url, whatsappData, whatsappConfig);
+        console.log(data);
+        res.status(200).send({
+          message: "Evento escuchado",
+        });
+      }
     } catch (error) {
       console.error(error);
       res.end();
