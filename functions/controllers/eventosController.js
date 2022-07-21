85a1a7c76b5aca29fdddbdbe9ae4a69c4254c199
pipeswk <afco1995@gmail.com>
2022-07-20 23:52:53 -0500

    Se agrega imagen real del pronostico y boton wsp

--- functions/controllers/eventosController.js
+++ functions/controllers/eventosController.js
@@ -103,13 +103,24 @@ const escucharEventos = async (req, res) => {
           compradores: admin.firestore.FieldValue
               .arrayUnion(...nuevosInscritos),
         });
+        const pronImgRef = db.collection("imgpronosticos");
+        const pronImg = await pronImgRef
+            .where("idPron", "==", documento.data().idProductoComprado).get();
+        if (pronImg.empty) {
+          console.log("Documento no encontrado");
+          return;
+        }
+        const imagen = [];
+        pronImg.forEach((doc) => {
+          imagen.push(doc.data().img_src);
+        });
         // Se envia mensaje de whatsapp
         const whatsappData = JSON.stringify({
           "messaging_product": "whatsapp",
           "to": `57${documento.data().telefono}`,
           "type": "template",
           "template": {
-            "name": "pron_with_img",
+            "name": "pronos_normal_img_and_url",
             "language": {
               "code": "es_MX",
             },
@@ -120,7 +131,7 @@ const escucharEventos = async (req, res) => {
                   {
                     "type": "image",
                     "image": {
-                      "link": "https://firebasestorage.googleapis.com/v0/b/jupi-e46aa.appspot.com/o/pronosticos%2FNHL.jpg?alt=media&token=276807f0-b780-460c-80a2-94f526300e17",
+                      "link": imagen[0],
                     },
                   },
                 ],
@@ -146,11 +157,30 @@ const escucharEventos = async (req, res) => {
                   },
                   {
                     "type": "text",
+                    "text": "sorteo",
+                  },
+                  {
+                    "type": "text",
                     "text": documento.data().refPago,
                   },
                   {
                     "type": "text",
                     "text": "ganar",
+                  },
+                  {
+                    "type": "text",
+                    "text": "sorteo",
+                  },
+                ],
+              },
+              {
+                "type": "button",
+                "sub_type": "url",
+                "index": 0,
+                "parameters": [
+                  {
+                    "type": "text",
+                    "text": sorteo.id,
                   },
                 ],
               },
@@ -196,6 +226,7 @@ const eventosMercadoPago = async (req, res) => {
       const url = `https://api.mercadopago.com/v1/payments/${req.body.data.id}`;
       const {data} = await axios.get(url, config);
       if (data.status === "approved") {
+        console.log("El resultado en MP es", data);
         const id = req.body.data.id;
         const ref = db.collection("transactions");
         const snap = await ref.where("idMercadoPago", "==", parseInt(id)).get();
@@ -221,15 +252,176 @@ const eventosMercadoPago = async (req, res) => {
           nuevosInscritos.push(`${docs[0].refPago}-${i}`);
         }
 
-        const sortRef = db.collection("sorteos")
-            .doc(docs[0].idSorteo);
-        await sortRef.update({
-          participantes: admin.firestore.FieldValue
-              .arrayUnion(...nuevosInscritos),
-        });
-        res.status(200).send({
-          message: "Evento escuchado",
-        });
+        if (docs[0].tipoCompra === "sorteo") {
+          const sortRef = db.collection("sorteos")
+              .doc(docs[0].idSorteo);
+          const sorteo = await sortRef.get();
+          await sortRef.update({
+            participantes: admin.firestore.FieldValue
+                .arrayUnion(...nuevosInscritos),
+          });
+          // Se envia mensaje de whatsapp
+          const whatsappData = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "to": `57${docs[0].telefono}`,
+            "type": "template",
+            "template": {
+              "name": "sort_with_img",
+              "language": {
+                "code": "es_MX",
+              },
+              "components": [
+                {
+                  "type": "header",
+                  "parameters": [
+                    {
+                      "type": "image",
+                      "image": {
+                        "link": sorteo.data().img,
+                      },
+                    },
+                  ],
+                },
+                {
+                  "type": "body",
+                  "parameters": [
+                    {
+                      "type": "text",
+                      "text": docs[0].nombreCliente,
+                    },
+                    {
+                      "type": "text",
+                      "text": docs[0].tipoCompra,
+                    },
+                    {
+                      "type": "text",
+                      "text": sorteo.data().nombre,
+                    },
+                    {
+                      "type": "text",
+                      "text": docs[0].cantidad,
+                    },
+                    {
+                      "type": "text",
+                      "text": docs[0].refPago,
+                    },
+                  ],
+                },
+              ],
+            },
+          });
+          const whatsappConfig = {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
+            },
+          };
+          const url = "https://graph.facebook.com/v13.0/106635852120380/messages";
+          const {data: r} = await axios.post(url, whatsappData, whatsappConfig);
+          console.log(r);
+          res.status(200).send({
+            message: "Evento escuchado",
+          });
+        } else {
+          const sortRef = db.collection("sorteos")
+              .doc(docs[0].idSorteo);
+          const sorteo = await sortRef.get();
+          await sortRef.update({
+            participantes: admin.firestore.FieldValue
+                .arrayUnion(...nuevosInscritos),
+          });
+          const pronRef = db.collection("pronosticos")
+              .doc(docs[0].idProductoComprado);
+          await pronRef.update({
+            compradores: admin.firestore.FieldValue
+                .arrayUnion(...nuevosInscritos),
+          });
+          // Se envia mensaje de whatsapp
+          const whatsappData = JSON.stringify({
+            "messaging_product": "whatsapp",
+            "to": `57${docs[0].telefono}`,
+            "type": "template",
+            "template": {
+              "name": "pronos_normal_img_and_url",
+              "language": {
+                "code": "es_MX",
+              },
+              "components": [
+                {
+                  "type": "header",
+                  "parameters": [
+                    {
+                      "type": "image",
+                      "image": {
+                        "link": "https://firebasestorage.googleapis.com/v0/b/jupi-e46aa.appspot.com/o/pronosticos%2FNHL.jpg?alt=media&token=276807f0-b780-460c-80a2-94f526300e17",
+                      },
+                    },
+                  ],
+                },
+                {
+                  "type": "body",
+                  "parameters": [
+                    {
+                      "type": "text",
+                      "text": docs[0].nombreCliente,
+                    },
+                    {
+                      "type": "text",
+                      "text": docs[0].tipoCompra,
+                    },
+                    {
+                      "type": "text",
+                      "text": "sorteo",
+                    },
+                    {
+                      "type": "text",
+                      "text": sorteo.data().nombre,
+                    },
+                    {
+                      "type": "text",
+                      "text": "sorteo",
+                    },
+                    {
+                      "type": "text",
+                      "text": docs[0].refPago,
+                    },
+                    {
+                      "type": "text",
+                      "text": "ganar",
+                    },
+                    {
+                      "type": "text",
+                      "text": "sorteo",
+                    },
+                  ],
+                },
+                {
+                  "type": "button",
+                  "sub_type": "url",
+                  "index": 0,
+                  "parameters": [
+                    {
+                      "type": "text",
+                      "text": sorteo.id,
+                    },
+                  ],
+                },
+              ],
+            },
+          });
+          const whatsappConfig = {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": `Bearer ${process.env.ACCESS_TOKEN_WHATSAPP}`,
+            },
+          };
+          const url = "https://graph.facebook.com/v13.0/106635852120380/messages";
+          const {data: r} = await axios.post(url, whatsappData, whatsappConfig);
+          console.log(r);
+          res.status(200).send({
+            message: "Evento escuchado",
+          });
+        }
       } else {
         res.status(200).send({
           message: "Evento escuchado, transacción no completada.",
