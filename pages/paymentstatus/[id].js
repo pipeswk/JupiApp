5326f8f3511dc39587c397e531396a1b161acbbc
pipeswk <afco1995@gmail.com>
2022-07-26 21:06:05 -0500

    Se elimina SSR para cambiarlo por SnapShot

--- pages/paymentstatus/[id].js
+++ pages/paymentstatus/[id].js
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import Layout from '../../src/Layout/Layout'
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, onSnapshot } from 'firebase/firestore';
 import { db } from '../../utils/Firebase'
 import StatusPse from '../../src/Components/StatusPse';
 
-const EntradaPago = ( { id, resultado } ) => {
+const EntradaPago = () => {
+    const [resultado, setResultado] = useState({});
+    const router = useRouter();
+    const { id } = router.query;
+    console.log(id);
+
+    useEffect(() => {
+        if (id !== undefined) {
+            const payStatus = onSnapshot(doc(db, 'transactions', id), (payment) => {
+                setResultado(payment.data());
+            });
+            return
+        }
+    }, [id])
+    
+
   return (
     <Layout
         pagina='Pago'
@@ -20,24 +36,24 @@ const EntradaPago = ( { id, resultado } ) => {
 
 export default EntradaPago
 
-export async function getServerSideProps( { params: {id} } ) {
-    let resultado;
-    const docRef = doc(db, 'transactions', id);
-    const documento = await getDoc(docRef);
-    if (documento.exists()) {
-        resultado = documento.data();
-    } else {
-        console.log('Documento no encontrado!');
-        return {
-            redirect: {
-              destination: '/',
-            }
-        }
-    }
-    return {
-        props: {
-        id,
-        resultado
-        }
-    }
-}
+// export async function getServerSideProps( { params: {id} } ) {
+//     let resultado;
+//     const docRef = doc(db, 'transactions', id);
+//     const documento = await getDoc(docRef);
+//     if (documento.exists()) {
+//         resultado = documento.data();
+//     } else {
+//         console.log('Documento no encontrado!');
+//         return {
+//             redirect: {
+//               destination: '/',
+//             }
+//         }
+//     }
+//     return {
+//         props: {
+//         id,
+//         resultado
+//         }
+//     }
+// }
