6e99051effd25058bc83386d36b0eb50379c4869
pipeswk <afco1995@gmail.com>
2022-07-26 21:06:16 -0500

    Se agrega Spinner

--- src/Components/StatusPse.jsx
+++ src/Components/StatusPse.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Spinner from './Spinner';
+import styles from '../../styles/StatusPse.module.css'
 
 const StatusPse = ( { data } ) => {
 
@@ -23,19 +25,18 @@ const StatusPse = ( { data } ) => {
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
                         </svg>
                     </div>
-                    <p className='text-center fw-bold'>{`Los datos del ${tipoCompra} han sido enviados a tu email y a tu whatsapp. Gracias por tu compra.`}</p>
+                    <p className='text-center fw-bold'>{`Los datos del ${tipoCompra} han sido enviados a tu whatsapp. Gracias por tu compra.`}</p>
+                    <span>En caso de que no recibas el mensaje en tu Whatsapp por favor revisa tu bandeja de mensajes de texto (SMS).</span>
                 </>
             ) : (
                 <>
                    <div className='d-flex justify-content-center'>
-                        <p className='text-danger fw-bold fs-3 my-4 text-center'>{`Tu transacción aun no se ha aprobado`}</p>
+                        <p className={`${styles.confirmando} fw-bold fs-3 my-4 text-center`}>Estamos confirmando el pago</p>
                     </div>
-                    <div className='d-flex justify-content-center'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" className="bi bi-exclamation-circle-fill text-danger mb-5" viewBox="0 0 16 16">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                        </svg>
+                    <div className='d-flex justify-content-center my-5'>
+                        <Spinner />
                     </div>
-                    <p className='text-center fw-bold'>Si ya hiciste el pago espera unos segundos y recarga esta pagina de nuevo</p> 
+                    <p className='text-center fw-bold'>Por favor espera un momento mientras confirmamos tu pago, esta operación puede durar unos minutos...</p> 
                 </>
             )}
         </div>
