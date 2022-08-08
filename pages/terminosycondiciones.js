ba95a6073e6f22323517c8d1385360860fabe433
pipeswk <afco1995@gmail.com>
2022-08-08 14:41:56 -0500

    Pagina Terminos y condiciones v1

---
+++ pages/terminosycondiciones.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import Layout from '../src/Layout/Layout'
+
+const Terminosycondiciones = () => {
+  return (
+    <Layout
+        pagina='Términos y condiciones'
+    >
+        <main>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-12'>
+                        <h1 className='text-center my-4'>Términos y condiciones</h1>
+                        <h3>1. POLÍTICA DE PRIVACIDAD</h3>
+                        <p>Esta política aplica a la información que recopilamos:</p>
+                        <ul>
+                            <li>En nuestro Sitio Web.</li>
+                            <li>En nuestras redes sociales (Facebook, Instagram, Telegram, Whatsapp, Discord, Twitch, entre otras...).</li>
+                            <li>En correos electrónicos, mensajes de texto (SMS).</li>
+                            <li>Formularios presenciales o información de proveedores aliados.</li>
+                        </ul>
+                        <p>ASEGÚRESE DE LEER ESTA POLÍTICA DETENIDAMENTE PARA COMPRENDER NUESTRAS POLÍTICAS Y PRÁCTICAS CON RESPECTO A SU INFORMACIÓN. SI NO ESTÁ DE ACUERDO CON NUESTRAS POLÍTICAS Y PRÁCTICAS, DEBERÁ ABSTENERSE DE UTILIZAR LA PÁGINA WEB Y/O APLICACIÓN. AL ACCEDER O UTILIZAR ESTA PÁGINA WEB Y/O APLICACIÓN, USTED ACEPTA ESTA POLÍTICA DE PRIVACIDAD. ESTA POLÍTICA DE PRIVACIDAD PUEDE CAMBIAR DE TIEMPO EN TIEMPO (CONSULTE LA PARTE DE CAMBIOS EN NUESTRA POLÍTICA DE PRIVACIDAD). SU USO CONTINUADO DE ESTE SITIO WEB Y/O APLICACIÓN AUN CUANDO SE HAYAN EFECTUADO CAMBIOS, SE ENTENDERÁ QUE USTED ACEPTA DICHOS CAMBIOS. POR LO TANTO, ES SU RESPONSABILIDAD REVISAR ESTA POLÍTICA DE PRIVACIDAD PERIÓDICAMENTE PARA OBTENER ACTUALIZACIONES.</p>
+
+                        <h4>1.1 Niños menores de 18 años</h4>
+                        <p>Nuestra Página Web no está dirigida para personas menores de 18 años. Ninguna persona menor de 18 años puede proveer información a, o en, el Sitio Web y/o Aplicación. La Compañía no recopila información de niños menores de 18 años de manera intencional. Si usted es un menor de 18 años, no use ni brinde ninguna información en este Sitio Web y/o Aplicación ni en ninguna de sus funciones. Si Nosotros descubrimos que hemos recopilado o recibido información personal de un menor de 18 años sin verificación del consentimiento de los padres, procederemos a eliminar dicha información. Si usted cree que la Compañía tiene a su haber información personal de un menor de 18 años, contáctenos a: admin@jupi.com.co</p>
+                        <h4>1.2 Información que recopilamos sobre usted y cómo la recopilamos.</h4>
+                        <ul>
+                            <li>Mediante la cual usted, como Usuario, puede ser identificado personalmente, como su nombre, apellido, dirección postal, dirección de correo electrónico, número de teléfono, o cualquier otra información que lo identifique mediante la cual usted pueda ser contactado ya sea en línea u offline (“data personal” o “información personal”). La información de identificación personal se refiere a la información que se recopila junto con su nombre.</li>
+                            <li>Información que es sobre usted pero que no lo identifica individualmente; y/o</li>
+                            <li>Sobre su conexión a Internet, el equipo que utiliza para acceder a nuestro sitio web y/o Aplicación y los detalles de uso.</li>
+                        </ul>
+                        <p>Recopilamos esta información:</p>
+                        <ul>
+                            <li>Nombres y apellidos</li>
+                            <li>Dirección postal</li>
+                            <li>Correo electrónico</li>
+                            <li>Numero de teléfono y/o Whatsapp</li>
+                            <li>Información de identificación personal</li>
+                            <li>Información sobre su conexión a Internet, el equipo que utiliza para acceder a nuestro sitio web y/o Aplicación y los detalles de uso</li>
+                        </ul>
+                        <p>¿Como recopilamos esa información?</p>
+                        <ul>
+                            <li>Mediante nuestro formulario de compra (Check-out) previo a la transacción de compra</li>
+                            <li>Mediante la cual usted, como Usuario, puede ser identificado personalmente, como su nombre, apellido, dirección postal, dirección de correo electrónico, número de teléfono, o cualquier otra información que lo identifique mediante la cual usted pueda ser contactado ya sea en línea u offline (“data personal” o “información personal”). La información de identificación personal se refiere a la información que se recopila junto con su nombre.</li>
+                            <li>Mediante formularios de campañas de marketing en redes sociales</li>
+                            <li>Mediante formularios de campañas de marketing en correos electrónicos</li>
+                            <li>Mediante formularios de campañas de marketing en mensajes de texto (SMS)</li>
+                            <li>Mediante formularios de campañas de marketing en formularios presenciales o información de proveedores aliados</li>
+                            <li>Información que Recopilamos a través de Tecnologías de Recopilación de Datos Automáticas (Pixel de facebook, Google analitycs, entre otras).</li>
+                        </ul>
+                        <h4>1.3 ¿Como usamos su información?</h4>
+                        <ul>
+                            <li>Nosotros usamos la información personal para:</li>
+                            <li>Para proveerle una mejor experiencia en nuestro sitio web y/o Aplicación</li>
+                            <li>Para registrar, inspeccionar, investigar y analizar errores de codificación en la aplicación y/o sitio web.</li>
+                            <li>Para responder a su solicitud, comentario, o pregunta</li>
+                            <li>Para brindarle información, productos o servicios que nos solicite.</li>
+                            <li>Para notificarle sobre cambios en nuestra Aplicación y/o sitio web o cualquier producto o servicio que ofrecemos o proveemos a través de ellos. Para permitirle participar en funciones interactivas en nuestra aplicación y/o sitio web.</li>
+                            <li>Para cualquier otro propósito con su consentimiento.</li>
+                            <li>También podemos utilizar su información para comunicarnos con usted acerca de servicios que puedan ser de su interés.</li>
+                        </ul>
+                        <h4>1.4 ¿Cómo protegemos su información?</h4>
+                        <p>Nuestra Página Web y/o Aplicación está protegida por las medidas de seguridad de la información personal que recopilamos. Estas medidas de seguridad incluyen:</p>
+                        <ul>
+                            <li>Hemos implementado medidas diseñadas para proteger su información personal de pérdidas accidentales y de acceso, uso, alteración y divulgación no autorizados. Todas las transacciones de pago se cifrarán mediante tecnología SSL y se subcontratarán a un canal de procesamiento de pagos de terceros, en el que cada transacción se regirá por los términos y condiciones de la empresa de procesamiento de pagos de terceros. Cuando usted se suscriba, usted será redirigido a un canal de procesamiento de terceros donde podrá completar su compra de forma independiente.</li>
+                            <li>Mantenimiento en reglas de seguridad de bases de datos y cambios periódicos de credenciales de acceso a las mismas</li>
+                            <li>Procesamiento de datos 100% a nivel de servidor</li>
+                        </ul>
+                        <p>Desafortunadamente, la transmisión de información a través de Internet no es completamente segura. Aunque hacemos todo lo posible para proteger su información personal, no podemos garantizar la seguridad de su información personal transmitida a nuestro sitio web y/o aplicación. Cualquier transmisión de información personal es bajo su propio riesgo. No somos responsables de la elusión de cualquier configuración de privacidad o medidas de seguridad contenidas en el sitio web y/o aplicación.</p>
+                        <h4>1.5 Divulgación de su información</h4>
+                        <p>Nuestra Página Web y/o Aplicación puede contener enlaces a otros sitios web. Si usted hace clic en un enlace a un sitio web que no es nuestro, nosotros no somos responsables de la protección o privacidad de la información que usted transmita o recibe en ese sitio web. Usted reconoce y acepta que nosotros no tenemos control sobre el contenido y la privacidad de los sitios web a los que usted puede acceder a través de nuestra Página Web y/o Aplicación.</p>
+                        <p>Podemos divulgar información agregada sobre nuestros usuarios sin restricción.</p>
+                        <p>Podemos divulgar la información personal que recopilamos o que usted provea como se describe en esta Política de Privacidad:</p>
+                        <ul>
+                            <li>PPara cumplir con cualquier orden judicial, ley o proceso legal, incluso para responder a cualquier solicitud gubernamental o reguladora</li>
+                            <li>Para proteger nuestros derechos o los de nuestros usuarios o de terceros</li>
+                            <li>Para prevenir o investigar actividades fraudulentas o malintencionadas</li>
+                            <li>Para proteger nuestros propios intereses</li>
+                            <li>A compradores, contratistas, proveedores de servicios y otros terceros que utilizamos para apoyar a nuestro negocio</li>
+                            <li>A un comprador u otro suceso en caso de fusión, adquisición, cesión, reestructuración, reorganización, disolución u otra venta o transferencia de algunos o todos los activos de Jupi S.A.S, ya sea como empresa en funcionamiento o como parte de una quiebra, liquidación, o procedimiento similar, en el que la información personal en poder de Jupi S.A.S. sobre los usuarios de nuestra aplicación y/o sitio web se encuentra entre los activos transferidos.</li>
+                        </ul>
+                        <h3>2. TÉRMINOS DE USO</h3>
+                        <p>Jupi S.A.S (“Jupi” “nosotros,” “nos,” “nuestro”) le brinda sus servicios (descritos a continuación) a través de su sitio web ubicado en www.jupi.com.co (el “Sitio” o “Sitio web”) y servicios relacionados (colectivamente, tales servicios, incluyendo las nuevas funciones y aplicaciones, y el Sitio, el (los) “Servicio(s)”), sujeto a los siguientes Términos de Uso (el “Acuerdo”). POR FAVOR LEA ESTE ACUERDO CUIDADOSAMENTE. AL HACER CLIC O PULSAR EN EL BOTÓN DE REGISTRO O REGISTRARSE O EMPEZAR O ACTUALIZAR O AL ACCEDER O UTILIZAR LOS SERVICIOS DE JUPI, USTED ACEPTA ESTAR OBLIGADO POR LOS TÉRMINOS DE ESTE ACUERDO, NO PUEDE ACCEDER NI UTILIZAR LOS SERVICIOS DE JUPI. ES POSIBLE QUE SE HAGAN CAMBIOS A ESTE ACUERDO DE TIEMPO EN TIEMPO. HACEMOS ESFUERZOS COMERCIALES RAZONABLES PARA NOTIFICARLE DE CUALQUIER ACTUALIZACIÓN DEL MATERIAL DE ESTE ACUERDO. A PESAR DE LO ANTERIOR, SU USO CONTINUADO DEL SERVICIO SE CONSIDERARÁ COMO ACEPTACIÓN DE LOS TÉRMINOS MODIFICADO O ACTUALIZADOS. COMO TAL, DEBE VERIFICAR CON FRECUENCIA PARA VER SI HEMOS ACTUALIZADO ESTE ACUERDO. SI NO ESTÁ DE ACUERDO CON LOS TÉRMINOS O CONDICIONES DE ESTE ACUERDO, NO USE EL SERVICIO.</p>
+                        <p className='fw-bold'>Usted no puede usar el Servicio y no puede aceptar este Acuerdo si no tiene dieciocho (18) años o más. Si acepta este Acuerdo, usted declara que tiene la capacidad legal para estar sujeto al mismo.</p>
+                        <h4>2.1 Servicios</h4>
+                        <p>Jupi ofrece ciertos sorteos (Sorteo(s)) en el Sitio o la Aplicación para tener la oportunidad de ganar y recibir recompensas. Estos Términos generales de uso (este “Acuerdo”) rige su acceso y uso de los Servicios y comienzan en la fecha en la que Usted accede a entrar en este Acuerdo y continúan hasta que Usted o JUPI los rescinda (el “Término”). "Usted" / "Cliente" significa cualquier tercero que utilice el Servicio, incluyendo, entre otros, personas o entidades.</p>
+                        <p><span className='fw-bold'>“Servicio (s)”</span> significa las promociones, sorteos, contenido, herramientas, servicios y productos proporcionados por JUPI a través de su sitio web www.jupi.com.co o cualquier otro sitio web o aplicación operado por nosotros e incluyendo, sin limitación, videos, gráficos, grabaciones, documentación, páginas vinculadas, características, datos, texto, publicaciones de blog, imágenes, fotografías, música, sonidos, mensajes, etiquetas, contenido, programación, software y servicios de aplicaciones.</p>
+                        <p>LOS PARTICIPANTES PUEDEN PARTICIPAR HACIENDO LA COMPRA DEL TICKET Y UTILIZANDO CUALQUIERA DE LOS MÉTODOS DE ENTRADA, PAGANDO PARA PARTICIPAR.</p>
+                        <p>Por la presente, usted y jupi acuerdan lo siguiente:</p>
+
+                        <h4>2.2. Uso del Servicio</h4>
+                        <p>El servicio de JUPI, y cualquier contenido visto a través de nuestro servicio, es únicamente para su uso personal y no comercial. Sujeto al cumplimiento por parte del Cliente de los términos y condiciones de este Acuerdo (como condición para las subvenciones a continuación), JUPI otorga al Cliente y el Cliente acepta una licencia no exclusiva, no transferible, no sublicenciable y revocable para acceder y usar el Sitio de JUPI. Excepto por la licencia limitada mencionada anteriormente, ningún derecho, título o interés será transferido a usted. Usted acepta no otorgar licencias, crear trabajos derivados, transferir, vender o revender cualquier información, contenido o servicio obtenido del Sitio y/o Aplicación.</p>
+                        <p>JUPI puede revocar su licencia en cualquier momento a su entera discreción. Tras dicha revocación, usted debe destruir de inmediato todo el contenido descargado u obtenido de otro modo a través del servicio, así como las copias de dichos materiales, ya sea que se realicen de acuerdo con este Acuerdo o de otra manera.</p>
+
+                    </div>
+                </div>
+            </div>
+        </main>
+    </Layout>
+  )
+}
+
+export default Terminosycondiciones
