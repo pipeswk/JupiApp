5e55840a63a79bfde9d5f027817bed250f051942
sswwkk <andres.cuellar@fuac.edu.co>
2023-03-21 17:40:50 -0500

    Se agrega funcion liberadora de lottos

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const moment = require("moment-timezone");
+const {db} = require("./controllers/nequiController.js");
 
 const app = express();
 const efectivo = express();
@@ -46,3 +48,29 @@ exports.app = functions.https.onRequest(app);
 exports.efectivo = functions.https.onRequest(efectivo);
 exports.eventos = functions.https.onRequest(eventos);
 exports.lottos = functions.https.onRequest(lottos);
+exports.liberador = functions.pubsub.schedule("every 15 minutes")
+    .onRun(async (context) => {
+      await db.runTransaction(async (t) => {
+        const sorteosDisponibles = await t.get(db.collection("sorteos").where("status", "==", "active"));
+        sorteosDisponibles.forEach(async (sorteo) => {
+          console.log(sorteo.data().nombre);
+          let newLottos = [];
+          newLottos = sorteo.data().lottos;
+          newLottos.map((lotto, i) => {
+            const now = moment().tz("America/Bogota");
+            const fechaReserva = moment(lotto.fechaReserva._seconds * 1000).tz("America/Bogota");
+            const diff = now.diff(fechaReserva, "minutes");
+            if (diff > 15 && lotto.avaliable === true) {
+              newLottos[i] = {
+                ...newLottos[i],
+                checkoutId: "",
+              };
+              t.update(sorteo.ref, {lottos: newLottos});
+              console.log(lotto.number + " liberado");
+            } else {
+              console.log("No se ha liberado el lotto" + lotto.number);
+            }
+          });
+        });
+      });
+    });
