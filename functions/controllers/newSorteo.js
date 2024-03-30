cb4d64c21e9f27f92fe0d1ed7e97d87cc53754e9
sswwkk <andres.cuellar@fuac.edu.co>
2024-03-30 15:07:36 -0500

    Enpoint creador de campañas

---
+++ functions/controllers/newSorteo.js
@@ -0,0 +1,44 @@
+const admin = require('firebase-admin');
+const db = admin.firestore();
+
+const newSorteo = async (req, res) => {
+    console.log('newSorteo');
+    const sorteo = req.body;
+    console.log(sorteo);
+    // Se crea el sorteo en la colección "sorteos"
+    try {
+        const idSorteo = db.collection('sorteos').doc().id;
+        const sortRef = db.collection('sorteos').doc(idSorteo);
+        await sortRef.set({
+            id: idSorteo,
+            capacidad: sorteo.total_quotas,
+            caracteristicas: [],
+            categoria: sorteo.category,
+            img: sorteo.images,
+            nombre: sorteo.title,
+            onPronosticos: false,
+            participantes: [],
+            preview_img: sorteo.video_placeholder,
+            status: "active",
+            valorTicket: sorteo.price,
+            video_poster: sorteo.video_placeholder,
+            video_src: sorteo.video_src,
+        });
+        res.status(200).send({
+            success: true,
+            message: 'Sorteo creado correctamente en Jupi',
+            sorteo: idSorteo,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error al crear el sorteo',
+            error: error,
+        });
+    }
+};
+
+module.exports = {
+    newSorteo,
+};
--- functions/routes/nequi.routes.js
+++ functions/routes/nequi.routes.js
@@ -3,10 +3,12 @@ const {
   ejecutarPagoSorteo,
   ejecutarPagoPronostico,
 } = require("../controllers/nequiController.js");
+const {newSorteo} = require("../controllers/newSorteo.js")
 
 const router = new express.Router();
 
 router.post("/procesar-pago-sorteo", ejecutarPagoSorteo);
 router.post("/procesar-pago-pronostico", ejecutarPagoPronostico);
+router.post("/new-sorteo", newSorteo);
 
 module.exports = router;
